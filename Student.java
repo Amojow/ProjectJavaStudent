f6eb1fc220748cc533d0a687e86e9d7df49ce6d7
ImaneDyani <32196406+ImaneDyani@users.noreply.github.com>
2017-10-28 14:45:04 +0200

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,86 +1,85 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Student {
-	public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setPhotoPath(String photoPath) {
-        this.photoPath = photoPath;
-    }
-
-    public void setMark(String mark) {
-        this.mark = mark;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    String firstName;
-
-    public String getSurName() {
-        return surName;
-    }
-
-    String surName;
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    String birthDate;
-
-    public String getGender() {
-        return gender;
-    }
-
-    String gender;
-
-    public String getPhotoPath() {
-        return photoPath;
-    }
-
-    String photoPath;
-
-    public Student(String firstName, String surName, String birthDate, String gender, String photoPath, String mark, String comment) {
-        this.firstName = firstName;
-        this.surName = surName;
-        this.birthDate = birthDate;
-        this.gender = gender;
-        this.photoPath = photoPath;
-        this.mark = mark;
-        this.comment = comment;
-    }
-
-    public String getMark() {
-        return mark;
-
-    }
-
-    String mark;
-
-    public String getComment() {
-        return comment;
-    }
-
-    String comment;
-
+	
+	String name;
+	String gender;
+	String surName;
+	String birthDate;
+	String photoPath;
+	String mark;
+	String comment;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getSurName() {
+		return surName;
+	}
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getPhotoPath() {
+		return photoPath;
+	}
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+	public String getMark() {
+		return mark;
+	}
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public Student(String name, String gender) {
+		super();
+		this.name = name;
+		this.gender = gender;
+	}
+	
+	public void saveStudent(){
+	Connection conn = DBManager.Connector();
+	if (conn==null) System.exit(1);
+	else {
+	PreparedStatement pst;
+	String insertString = "INSERT INTO Student (Name, Gender) VALUES (?, ?)";
+	try {
+	pst = conn.prepareStatement(insertString);
+	String name = this.name;
+	String gender = this.gender;
+	pst.setString(1, name);
+	pst.setString(2, gender);
+	pst.executeUpdate();
+		 
+		} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+		}
+		}
 
 }
